859b0c2e004676735772e479492b7f2601eefd30
crz29 <crz29@hotmail.com>
2023-05-02 01:34:38 -0300

    cambios

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const input = document.getElementById("input");
 const btn_todo = document.getElementById("btn_todo");
 const container_todo = document.querySelector(".container_tareas");
 
+const container_completadas = document.querySelector(".container_completadas");
+
 
 let id=1;
 /*
@@ -55,8 +57,7 @@ function addTodo(){
 
    function eliminarTarea(id){
         document.getElementById(id).parentElement.parentElement.remove();
-        const elimino = true;
-
+ 
    };
 
 
@@ -64,7 +65,6 @@ function addTodo(){
    checkbox.addEventListener("click",()=>{
             if (checkbox.checked){
                 button.addEventListener("click", ()=>{
-                let recibeTarea = parrafo.innerHTML;
                 eliminarTarea(button.getAttribute("id"));
             });
         };
@@ -79,5 +79,47 @@ btn_todo.addEventListener("click",()=>{
 
 });
 
-export default script();
+/*
+           <div class="container_completa">
+                <div class="container_completa-">
+                    <input type="checkbox" class="box">
+                    <p class="tarea_ingresada">Tarea Ingresada...</p>
+                </div>
+                <div class="container_list_boton">
+                    <button class="btn-eliminar" ><i class="fa-regular fa-square-minus"></i></button>
+                </div>
+            </div>
 
+function addCompl(){
+    //creo el containerCompletas con atributos y clases
+    const div_compl = document.createElement("div");
+    const div_completa = document.createElement("div");
+    const parrafo2 = document.createElement("p");
+    const div_boton2 = document.createElement("div")
+    const button2 = document.createElement("button");
+    const icono2 = document.createElement("i");
+
+    //le doy estilo a los elementos
+    parrafo2.innerHTML=parrafo
+
+    div_compl.classList.add("container_completa");
+    div_completa.classList.add("container_listado-");
+    parrafo2.classList.add("tarea_ingresada");
+    div_boton2.classList.add("container_list_boton");
+    button2.classList.add("btn-eliminar");
+    
+
+    icono2.classList.add("fa-regular", "fa-square-minus");
+     //Se forma todo la fila tarea
+    div_compl.appendChild(div_completa);
+    div_compl.appendChild(div_boton2);
+    div_completa.appendChild(parrafo2);
+    div_boton2.appendChild(button2);
+    button2.appendChild(icono2);
+
+   //se agrega todo la fila tarea
+   container_completadas.appendChild(div_compl);
+
+};
+
+*/
